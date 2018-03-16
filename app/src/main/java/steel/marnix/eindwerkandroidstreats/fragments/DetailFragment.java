1db0226be94b0c1df2c33126864d80d2ccf7ac98
marnixsteel <marnix.steel@icloud.com>
2018-03-16 11:44:06 +0100

    Initial commit

---
+++ app/src/main/java/steel/marnix/eindwerkandroidstreats/fragments/DetailFragment.java
@@ -0,0 +1,34 @@
+package steel.marnix.eindwerkandroidstreats.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import steel.marnix.eindwerkandroidstreats.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailFragment extends Fragment {
+
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+    public static DetailFragment newInstance(){
+        return new DetailFragment();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+}
